1e34fd3ca9d4724f4e5ac6d1f0e202b977ed08c2
Pasindu Wilagama <pasidubhagya20@gmail.com>
2025-07-05 01:24:53 +0530

    Sub menu and FoodCard component

--- src/Pages/FoodsHome/FoodsHome.jsx
+++ src/Pages/FoodsHome/FoodsHome.jsx
@@ -3,12 +3,14 @@ import Navbar from "../../Components/Navbar/Navbar";
 import Footer from "../../Components/Footer/Footer";
 import ContactUs from "../../Components/ContactUs/ContactUs"; // Assuming you have a ContactUs component
 import "./FoodsHome.css"; // Assuming you have a CSS file for styling
+import FoodCard from "../../Components/FoodCard/FoodCard";
 
 function FoodsHome() {
   return (
     <>
       <Navbar />
       <div className="foodsHomeContainer">
+        <FoodCard />
         <ContactUs />
       </div>
 
