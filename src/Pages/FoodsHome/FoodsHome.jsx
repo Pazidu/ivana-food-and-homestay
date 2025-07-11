f6e03d259c24cb6235f5d9782d07c913e6519c9b
Pasindu Wilagama <pasidubhagya20@gmail.com>
2025-07-11 11:23:56 +0530

    Add image carousel

--- src/Pages/FoodsHome/FoodsHome.jsx
+++ src/Pages/FoodsHome/FoodsHome.jsx
@@ -4,13 +4,16 @@ import Footer from "../../Components/Footer/Footer";
 import ContactUs from "../../Components/ContactUs/ContactUs"; // Assuming you have a ContactUs component
 import "./FoodsHome.css"; // Assuming you have a CSS file for styling
 import FoodCard from "../../Components/FoodCard/FoodCard";
+import Slidebar from "../../Components/Slidebar/Slidebar";
 
 function FoodsHome() {
   return (
     <>
       <Navbar />
+
       <div className="foodsHomeContainer">
-        <ContactUs />
+        <Slidebar />
+        {/* <ContactUs /> */}
       </div>
 
       <Footer />
