57a4a66ef95d67925857af8ef44fc55c29f97e5b
Pasindu Wilagama <pasidubhagya20@gmail.com>
2025-08-31 19:47:18 +0530

    Foods Feedback put reviews and show

--- src/Pages/User/Home.jsx
+++ src/Pages/User/Home.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function Home() {
-  return <div>Hello User</div>;
-}
+const Home = () => {
+  const navigate = useNavigate();
+  const fetchUser = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.get("http://localhost:5000/api/user", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.status !== 201) {
+        navigate("/login");
+      }
+    } catch (error) {
+      navigate("/login");
+      console.log(error);
+    }
+  };
+  fetchUser();
+  return (
+    <div>
+      <h1>Hello User</h1>
+    </div>
+  );
+};
 
 export default Home;
