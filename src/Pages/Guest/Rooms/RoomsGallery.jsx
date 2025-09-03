97583fa2d549899b5fad6020ec0d92592ba30308
MahazenD <164654780+MahazenD@users.noreply.github.com>
2025-09-03 20:09:33 +0530

    create ivana homestay structure

---
+++ src/Pages/Guest/Rooms/RoomsGallery.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const RoomsGallery = () => {
+  return (
+    <div>
+      <h2>Rooms Gallery</h2>
+      <p>Photos of our homestay rooms will be shown here.</p>
+    </div>
+  );
+};
+
+export default RoomsGallery;
