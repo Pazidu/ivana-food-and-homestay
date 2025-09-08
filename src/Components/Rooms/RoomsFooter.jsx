633e115caa53ac399a6f63aeb5b13a1796cb34cb
MahazenD <164654780+MahazenD@users.noreply.github.com>
2025-09-08 22:35:48 +0530

    set map and footer

--- src/Components/Rooms/RoomsFooter.jsx
+++ src/Components/Rooms/RoomsFooter.jsx
@@ -14,13 +14,13 @@ const RoomsFooter = () => {
             <FaMapMarkerAlt /> Ivana Food and Homestay, Kandy, Sri Lanka
           </p>
           <iframe
-            title="Ivana Location"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63319.517229853!2d79.991!3d9.661!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3afe54d4cfdcb5e7%3A0x1d59b2f5ec9d5f5!2sJaffna!5e0!3m2!1sen!2slk!4v1694000000000"
-            width="100%"
-            height="200"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.5803633698642!2d80.66677497465189!3d7.28849209271893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae3670079f8e0b1%3A0x455a50a07a333431!2sIvana%20Food%20Court!5e0!3m2!1sen!2slk!4v1749564798460!5m2!1sen!2slk"
+            width="450"
+            height="250"
             style={{ border: 0 }}
-            allowFullScreen=""
+            allowFullScreen
             loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
 
