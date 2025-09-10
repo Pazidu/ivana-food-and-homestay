b7496ee6a80a1b4c0975859d2c9f7190017dbc01
MahazenD <164654780+MahazenD@users.noreply.github.com>
2025-09-11 00:04:32 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,7 @@ function App() {
           path="/user/profile"
           element={
             <ProtectedRoute role="user">
+
               <Profile />
             </ProtectedRoute>
           }
